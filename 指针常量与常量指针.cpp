9468b772e2f12a1d8366bc8a3f7a0ae62be1cbfd
wenke.xu <wenke.xu@hirain.com>
2025-05-30 11:17:07 +0800

    指针常量与常量指针

--- 指针常量与常量指针.cpp
+++ 指针常量与常量指针.cpp
@@ -21,3 +21,15 @@ int main()
 
 
 }
+
+/*
+常量指针与指针常量的记忆方法
+
+读法：
+    const double *ptr;     //const读作常量，*读作指针，按照顺序读作常量指针
+    double * const ptr;     //const读作常量，*读作指针，按照顺序读作指针常量
+
+    const靠近*还是double,那么谁就是不变的。
+    const靠近谁，谁不变，靠近*则指针不可变，靠近double则数不可变
+
+*/
